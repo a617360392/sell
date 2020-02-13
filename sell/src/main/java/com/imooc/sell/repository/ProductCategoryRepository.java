8ae882579ab845973f2d17461ad025fb3a074930
OneOnePeng <617360392@qq.com>
2020-02-13 22:29:16 +0800

    Initial commit

---
+++ sell/src/main/java/com/imooc/sell/repository/ProductCategoryRepository.java
@@ -0,0 +1,11 @@
+package com.imooc.sell.repository;
+
+import com.imooc.sell.dataobject.ProductCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ */
+public interface ProductCategoryRepository extends JpaRepository<ProductCategory,Integer> {
+
+}
